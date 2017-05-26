d2cf3c7c470271ec938f499cb35f3000a81316a0
Victor Homyakov <vkhomyackov@gmail.com>
2017-05-26 16:19:04 +0300

    Clean up code

--- src/transifex-to-github.js
+++ src/transifex-to-github.js
@@ -25,13 +25,19 @@ var retrieveTransifexTranslations = (transifexApi) => {
         })));
 };
 
-module.exports = function (config) {
+/**
+ * @param {{github, transifex}} config
+ * @returns {Promise.<String>}
+ */
+module.exports = function update(config) {
     var gitRepo = git(config.github);
     var transifexApi = transifex(config.transifex);
     var getRepositoryLanguages = gitRepo
         .initRepo()
         .then(() => gitRepo.getLanguagesFromRepository());
-    return Promise.all([retrieveTransifexTranslations(transifexApi), getRepositoryLanguages])
+
+    return Promise
+        .all([retrieveTransifexTranslations(transifexApi), getRepositoryLanguages])
         .then(function ([translations, languagesInGit]) {
             var missingLanguages = _.difference(languagesInGit, _.pluck(translations, 'languageCode'));
             var languagesInfo = translations.map(({languageCode, name, stats}) => ({
@@ -43,19 +49,9 @@ module.exports = function (config) {
                 .concat(missingLanguages.map(x => gitRepo.removeLanguageFromGit(x)))
                 .concat(translations.map(({languageCode, resource}) => gitRepo.addDictionaryToGit(languageCode, resource)));
         })
-        .then(function () {
-            return gitRepo.getStatus();
-        })
+        .then(() => gitRepo.getStatus())
         .then(function (status) {
-            var hasChange = _.reduce(status, function (hasChange, st) {
-                return hasChange || Boolean(st.length);
-            }, false);
-            if (hasChange) {
-                return gitRepo.updatePackageVersion().then(function (res) {
-                    return res;
-                });
-            } else {
-                return "No update";
-            }
-        })
+            var hasChange = _.reduce(status, (hasChange, st) => hasChange || Boolean(st.length), false);
+            return hasChange ? gitRepo.updatePackageVersion() : 'No update';
+        });
 };
